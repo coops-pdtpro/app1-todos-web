808d22a5a7f674cf040fbd21fef0c6b90998ef5c
coops-pdtpro <cooper-da@hotmail.com>
2024-12-11 09:31:12 +0000

    limited characters and number of todos

--- web.py
+++ web.py
@@ -4,17 +4,22 @@ import functions
 todos = functions.get_todos()
 
 def add_todo():
-    todo = st.session_state["new_todo"] + "\n"
-    if todo in todos:
+    todo = st.session_state["new_todo"].strip()
+    if len(todos) >= 12:
+        st.error("You can only have up to 12 items in your list")
+    elif len(todo) > 80:
+        st.error("Todo item cannot exceed 80 characters")
+    elif todo in todos:
         st.error("This item is already in your list")
     else:
-        todos.append(todo)
+        todos.append(todo + "\n")
         functions.write_todos(todos)
     st.session_state["new_todo"] = ""
 
-st.title("My Todo App")
-st.subheader("This is my todo app")
-st.write("This app is to increase your productivity")
+st.title("Coops Todo App")
+st.subheader("Simple todo app to help with your tasks")
+st.write("This app is to increase your productivity, simply type in a new todo item and hit enter, "
+         "once completed simply check the tickbox to remove it from your list.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=f"{todo}_{index}")
